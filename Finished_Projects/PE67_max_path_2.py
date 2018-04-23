2af284dd14eecb30d4691cc80ebc6f45283a4a22
dylan-gregory-1011 <38666413+dylan-gregory-1011@users.noreply.github.com>
2018-04-23 16:52:42 -0400

    Update PE67_max_path_2.py

--- Finished_Projects/PE67_max_path_2.py
+++ Finished_Projects/PE67_max_path_2.py
@@ -1,17 +1,36 @@
-#finished on 6/19/2017
-def main():
-    maximum_path = []
-    #open the text file and download the numbers
-    with open('C:\\Users\\dylan smith\\Documents\\Python\\Project_Euler\\p067_triangle.txt') as triangle:
+#!/usr/bin/env python
+"""finished on 6/19/2017: Gets the maximum value returned from a triangle path
+that is laid out in the text file
+
+Cycle through each number in the row.  If the number is the first or last number,
+increment the total by the value of the previous number if the value is somewhere in 
+between then check to see which of the previous numbers is larger and increment that one.
+"""
+
+__author__ = "Dylan Smith"
+__copyright__ = "Copyright (C) 2017 Dylan Smith"
+__credits__ = ["Dylan Smith"]
+
+__license__ = "Public Domain"
+__version__ = "1.0"
+__maintainer__ = "Dylan Smith"
+__email__ = "-"
+__status__ = "Development"
+
+file_path = '\\PATH\\TO\\FILE'
+
+def getTriangleFromFile(file_path):
+    with open(file_path + 'p067_triangle.txt') as triangle:
         for row in triangle:
             row = row.strip('\n').split(' ')
             new_row = [int(x) for x in row]
             maximum_path.append(new_row)
+    return maximum_path
+if __name__ == '__main__':
+    maximum_path = getTriangleFromFile(file_path)
 
     #cycle through each row in the sheet
     for x in xrange(1,len(maximum_path)):
-        #cycle through each number in the row.  If the number is the first or last number, increment the total by the value of the previous number
-        #if the value is somewhere inbetween then check to see which of the previous numbers is larger and increment that one.
         for y in xrange(0,len(maximum_path[x])):
             if y == 0:
                 maximum_path[x][y] += maximum_path[x-1][y]
@@ -24,7 +43,3 @@ def main():
                     maximum_path[x][y]+=maximum_path[x-1][y-1]
 
     print max(maximum_path[len(maximum_path)-1])
-
-
-
-main()
